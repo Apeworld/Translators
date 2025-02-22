c0cd60a33f18aecbfb35a600c301fe7b06c235f6
Apeworld <122353037+Apeworld@users.noreply.github.com>
2025-02-21 21:53:45 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './main.css';  // main.css를 사용하는 경우
+// import './index.css';  // index.css를 사용하는 경우
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
