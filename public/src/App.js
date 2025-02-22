c4707747da588728e0576038987fd921eddf8fb4
Apeworld <122353037+Apeworld@users.noreply.github.com>
2025-02-21 20:46:55 -0800

    Add files via upload

---
+++ public/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WhisperTranslator from './components';
+
+function App() {
+  return (
+    <div className="App">
+      <WhisperTranslator />
+    </div>
+  );
+}
+
+export default App;
